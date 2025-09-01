9bda84dd3ac3775d7807c912fed2e46def4d39ed
vlltx <vlltx@tuta.io>
2025-09-01 15:43:57 +0300

    Add Utils.c

---
+++ KSystemExtender/Utils.c
@@ -0,0 +1,18 @@
+#include "Kse.h"
+
+PVOID RtlFindPattern(
+  _In_ PVOID Source,
+  _In_ SIZE_T
+  _In_ SourceLength,
+  _In_ PVOID Pattern,
+  _In_SIZE_T PatternLength
+)
+{
+	SIZE_T Index;
+
+	for (Index = 0; Index <= SourceLength - PatternLength; ++Index) {
+		if (RtlCompareMemory((PUINT8)(Source)+Index, Pattern, PatternLength) == PatternLength) return (PUINT8)(Source)+Index;
+	}
+
+	return NULL;
+}
